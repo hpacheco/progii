de6f61f230820eb657c19421585da454f47ba354
Hugo Pacheco <hpacheco@gmail.com>
2021-03-04 01:07:02 +0000

    teste0

---
+++ scripts/aula10_numpy_excel.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_excel('../dados/PT100-tx-tn-prec.xlsx',sheet_name=3)
+
+dados = df.to_numpy()
+print(dados.shape)
+
+anual = dados[:89,[0,17]]
+print(anual)
+
+min_prec = anual[:,1].min()
+max_prec = anual[:,1].max()
+
+print(anual[anual[:,1] == min_prec])
+print(anual[anual[:,1] == max_prec])
+
+xx = anual[anual[:,0] < 2000]
+print(xx)
+xx_prec = np.mean(xx[:,1])
+print(xx_prec)
+
+xxi = anual[anual[:,0] >= 2000]
+xxi_prec = np.mean(xxi[:,1])
+print(xxi_prec)
+
+
+
+
