ccf0861c7bf56009c6264c523dca8f46ff55a0b9
Hugo Pacheco <hpacheco@gmail.com>
2023-03-05 21:53:25 +0000

    t06

---
+++ scripts/aulas/6/1_download.py
@@ -0,0 +1,9 @@
+import urllib.request
+
+url = 'http://www.gutenberg.org/cache/epub/3333/pg3333.txt'
+urllib.request.urlretrieve(url,'../../../dados/lusiadas.txt')
+
+with open('../../../dados/lusiadas.txt','r') as f:
+    lines = f.readlines()
+for line in lines:
+    print(line)
---
+++ scripts/aulas/6/2_lusiadas_estrofes.py
@@ -0,0 +1,92 @@
+#lê ficheiro como lista de linhas sem \n
+with open('../../../dados/lusiadas.txt','r') as f:
+    lines = f.read().splitlines()
+
+# cria lista de estrofes
+estrofes = []
+for i,line in enumerate(lines):
+    if line.isnumeric():
+        estrofes.append(lines[i+1:i+9])
+
+#print(estrofes)
+
+# imprime estrofes
+for estrofe in estrofes:
+    for verso in estrofe: print(verso)
+    print()
+
+#numero de estrofes
+print(len(estrofes))
+
+#numero de versos
+numversos = 0
+for estrofe in estrofes:
+    numversos += len(estrofe)
+print(numversos)
+
+#numero de versos (ordem superior)
+print(sum(map(len,estrofes)))
+
+# remove espaços e caracteres não alfanuméricos num verso
+def rem_verso(verso):
+   for c in verso:
+       if not c.isspace() and not (c.isalpha() or c.isalnum()):
+           verso = verso.replace(c, '')
+   return verso
+
+import re
+
+# remove espaços e caracteres não alfabéticos num verso (expressões regulares)
+# \W: qualquer caracter não alfanumérico
+# \s: qualquer caracter de espaçamento
+def rem_verso2(verso):
+    return re.sub("(\W|\s)","",verso)
+
+# remove caracteres especiais em cada verso das estrofes
+# modificar listas in-place, cópia de strings
+for estrofe in estrofes:
+   for i,verso in enumerate(estrofe):
+       estrofe[i] = rem_verso(verso)
+
+# remove caracteres especiais em cada verso das estrofes
+# ordem superior
+rem_versos = lambda versos : list(map(rem_verso,versos))
+estrofes2 = list(map(rem_versos,estrofes))
+#print(estrofes2)
+
+#escreve estrofes para um ficheiro
+with open('estrofes.txt','w') as f:
+   for estrofe in estrofes:
+       for verso in estrofe:
+#            um verso por linha
+           f.write(verso+"\n")
+#        linha de espaço entre estrofes
+       f.write("\n")
+
+# versão imperativa
+npalavras = 0
+for estrofe in estrofes:
+   for verso in estrofe:
+       npalavras += len(verso.split())
+
+# versão funcional
+def sum_verso(verso):
+   return len(verso.split())
+def sum_estrofe(estrofe):
+   return sum(map(sum_verso,estrofe))
+npalavras = sum(map(sum_estrofe,estrofes))
+
+print(npalavras)
+
+import statistics
+
+# versão imperativa
+npalavras = []
+for estrofe in estrofes:
+    npalavras.append(sum_estrofe(estrofe))
+avg = statistics.mean(npalavras)
+
+# versão funcional
+avg = statistics.mean(map(sum_estrofe,estrofes))
+
+print(avg)
